088767d174ecab3e88ce519a4e0e57b8435cf97d
Nataliia Podpriatova <natalia.podpryatova@gmail.com>
2020-10-15 09:57:50 +0300

    add new configuration for eslint

---
+++ src/app.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const http = require('http');
+
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const path = require('path');
+
+const { config } = require('./config');
+
+const serverRequestLimit = rateLimit({
+    windowMs: config.serverRateLimits.period,
+    max: config.serverRateLimits.maxRequests
+});
+
+const app = express();
+const server = http.createServer(app);
+
+global.appRoot = path.resolve(process.cwd(), '../');
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(serverRequestLimit);
+
+const configureCors = (origin, callback) => {
+    const whiteList = config.ALLOWED_ORIGIN.split(';');
+
+    if(!origin) { // FOR POSTMAN
+        return callback(null, true);
+    }
+
+    if(!whiteList.includes(origin)) {
+        return callback(new Error('Cors not allowed'), false);
+    }
+
+    return callback(null, true);
+};
+app.use(cors({
+
+    origin: configureCors
+}));
+
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(global.appRoot, 'public')));
+
+app.use(morgan('dev'));
+app.use(helmet());
+
+const mountRoutes = () => {
+// TODO make routes
+};
+mountRoutes();
+
+const customErrorHandler = (err, req, res, next) => {
+    res
+        .status(err.status || 500)
+        .json({
+            message: err.message || 'Unknown Error',
+            code: err.code
+        });
+    next();
+};
+app.use(customErrorHandler);
+
+async function start() {
+    try {
+        await mongoose.connect(config.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        mongoose.connection.on('error', console.log.bind(console, 'MONGO ERROR'));
+        server.listen(config.PORT, () => {
+            console.log(`Listen ${config.PORT}`);
+        });
+    } catch (e) {
+        console.log('Server Error', e.message);
+        process.exit(1);
+    }
+}
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+        process.exit(0);
+    });
+});
+
+process.on('uncaughtException', (error) => { console.log(error); });
+
+process.on('unhandledRejection', (error) => { console.log(error); });
+
+start();
---
+++ src/config/config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    PORT: 5000,
+    HOST: 'http://localhost',
+    FRONTEND_URL: 'http://localhost:4200' || 'http://localhost:5000',
+
+    ALLOWED_ORIGIN: 'http://localhost:4200;http://localhost:3000',
+
+    CRON_JOB_PERIOD: '0 0 * * *',
+
+    JWT_SECRET: 'uf7e^WaiUGFSA7fd8&^dadh',
+    JWT_SECRET_ADMIN: 'uf7e^WaiUGFSA7fd8&^dadhADMIN',
+    ACCESS_TOKEN_LIFETIME: '20m',
+
+    JWT_REFRESH_SECRET: '3fhfsdjfkf$$uIEFSHFKdf',
+    JWT_REFRESH_SECRET_ADMIN: '3fhfsdjfkf$$uIEFSHFKdfADMIN',
+    REFRESH_TOKEN_LIFETIME: '1h',
+
+    JWT_CONFIRM_EMAIL_SECRET: 'd%^&fsdnFFkmsdkfHJFAJa',
+    JWT_CONFIRM_EMAIL_LIFETIME: '24h',
+
+    JWT_PASS_RESET_SECRET: '4234&&34refFSDJNK7sdf$%^',
+    JWT_PASS_RESET_LIFETIME: '24h',
+
+    serverRateLimits: {
+        period: 15 * 60 * 1000, // 15 minutes
+        maxRequests: 1000
+    },
+
+    MONGODB_URL: 'mongodb://localhost/smakolyky_shop',
+
+    ROOT_EMAIL: 'mail',
+    ROOT_EMAIL_PASSWORD: 'password',
+    ROOT_EMAIL_SERVICE: 'gmail',
+
+    SUPPORT_EMAIL: 'mail'
+};
